34788aa6b2a8788ace12d1349e31d98622f3e87a
Andrzej Jabłoński <64368904+Andreluss@users.noreply.github.com>
2021-09-08 10:29:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+print("assadfsdf")
+class State:
+    def __init__(self):
+        pass
+    """hahaha"""
+    def next_state(self, move):
+        pass
+    def possible_moves(self):
+        pass
+    def probability(self):
+        pass
+    def evaluate(self):
+        pass
+
+class Move:
+    pass
+
+max_depth = 4
+def minmax(state, depth):
+    if depth == max_depth:
+        return state.evaluate()
+    value = 0
+    for move in state.possible_moves():
+        value = max(value, 1-minmax(state.next_state(move), depth+1))
+    return value
+        
